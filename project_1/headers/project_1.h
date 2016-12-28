08112ff762c75c15162fb4cd7a059b7d0dcbee8c
vija5019 <vija5019@colorado.edu>
2016-12-27 21:47:56 -0700

    adding header file

---
+++ project_1/headers/project_1.h
@@ -0,0 +1,26 @@
+/********************************************
+*   Authors: Arundhathi Swami, Vignesh Jayaram
+*   Date Edited: 19 Sept 2016
+*
+*   File: project_1.h
+*
+*   Description: Header file for final project_report
+*		 -project_1_report
+*
+*
+********************************************************
+
+/*header for function called "project_1_report"*/
+
+
+#ifndef project_1_INCLUDED
+#define project_1_H_INCLUDED
+
+#include <stdint.h>
+#include "memory.h"
+#include "data.h"
+
+
+	void project_1_report();
+
+#endif
