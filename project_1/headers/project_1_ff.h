bddfe2df41d1f878515ccc113ac6620b79e5ae62
vija5019 <vija5019@colorado.edu>
2016-12-27 21:45:27 -0700

    adding header file for FRDM

---
+++ project_1/headers/project_1_ff.h
@@ -0,0 +1,23 @@
+/********************************************
+*   Authors: Arundhathi Swami, Vignesh Jayaram
+*   Date Edited: 19 Sept 2016
+*
+*   File: project_1_ff.h
+*
+*   Description: Header file for final project_report 
+*		 for FRDMKL25z Board
+*                -project_1_report
+*
+********************************************************
+
+/*header for function called "project_1_report"*/
+
+
+#ifndef project_1_INCLUDED
+#define project_1_H_INCLUDED
+
+#include <stdint.h>
+
+	void project_1_report();
+
+#endif
