b87cefb1b1c9dcb7de345a8ec53324cbe4a1cf76
vija5019 <vija5019@colorado.edu>
2016-12-27 21:55:54 -0700

    adding header file for unit tests of circular buffer

---
+++ project_2/header/CBUNITTESTS.h
@@ -0,0 +1,34 @@
+/********************************************
+*   Authors: Arundhathi Swami, Vignesh Jayaram
+*   Date Edited: 14 Oct 2016
+*
+*   File: CBUNITTESTS.h
+*
+*   Description: header file for test cases of circular buffer
+
+********************************************************/
+
+
+#ifndef INCLUDES_CBUNITTESTS_H_
+#define INCLUDES_CBUNITTESTS_H_
+
+#include <stdlib.h>
+#include "BUFFER.h"
+#include "MKL25Z4.h"
+#include "system_MKL25Z4.h"
+#include <stdint.h>
+#include "UART0_INIT.h"
+#include "LOGGER.h"
+#include "BUFFER.h"
+
+
+void TEST1 (void);
+void TEST2 (void);
+void TEST3 (void);
+void TEST4 (void);
+void TEST5 (void);
+
+
+
+
+#endif /* INCLUDES_CBUNITTESTS_H_ */
