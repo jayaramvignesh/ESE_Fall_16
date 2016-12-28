86b0d9ad406055d0e0fe14704141a1b892591da7
vija5019 <vija5019@colorado.edu>
2016-12-27 22:22:18 -0700

    adding source file for code profiling

---
+++ project_4/sources/profiling.c
@@ -0,0 +1,48 @@
+/*
+ 	    Filename: profiling.c
+ *      Description:  This source file contains the functions for profiling.
+ *
+ *
+ *  Created on: Oct 30, 2016
+ *      Author: Arundhathi Swami, Vignesh Jayaram
+ */
+
+#include "MKL25Z4.h"
+#include "profiling.h"
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+
+void counter()
+{
+
+	SIM->SCGC6 |= 0x01000000;            //initializing TPM0
+	SIM->SOPT2 |= 0x01000000;           //Selecting FLL clock
+	TPM0->SC = 0x0;                     //Disabling everything
+	TPM0->MOD = 0x0000FFFF;             //Setting Mod value to 65535
+	TPM0->CNT = 0x0;                    //making count 0
+	TPM0->SC = 0X00000080;              //counter overflow
+}
+
+//void counter_start()
+//{
+//	TPM0->CNT = 0x0;                    //making count 0
+//	TPM0->SC = 0x00000008;              //selecting mode for count
+//}
+//
+//void counter_stop()
+//{
+//	TPM0->SC = 0x00000000;                 //disabling the counter
+//
+//}
+
+double counter_calculate(void)
+{
+	cpu_ticks = TPM0->CNT;
+	time_elapsed= cpu_ticks*CLOCK_RESOLUTION;
+	return time_elapsed;
+}
+
